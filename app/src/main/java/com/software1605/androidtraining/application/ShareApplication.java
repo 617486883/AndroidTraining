200e66a1a51d6061ef40d7315f06e9dda3cd0489
617486883 <33947582+617486883@users.noreply.github.com>
2018-07-05 16:18:15 +0800

    李林山

---
+++ app/src/main/java/com/software1605/androidtraining/application/ShareApplication.java
@@ -0,0 +1,13 @@
+package com.software1605.androidtraining.application;
+
+import android.app.Application;
+
+public class ShareApplication extends Application {
+    private static final String APP_KEY = "193e71d0869ce";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
--- app/src/main/java/com/software1605/androidtraining/fragment/HomeFragment.java
+++ app/src/main/java/com/software1605/androidtraining/fragment/HomeFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -21,6 +22,8 @@ import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.software1605.androidtraining.R;
 
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 /**
  * 主页
  */
@@ -29,6 +32,7 @@ public class HomeFragment extends Fragment implements LocationSource,AMapLocatio
     AMapLocationClient mlocationClient;
     AMapLocationClientOption mLocationOption;
     private LatLng mLocalLatlng;
+    private TextView fenxiang;
     private boolean isFirst = true;
     MapView mapView;
     AMap aMap = null;
@@ -37,6 +41,13 @@ public class HomeFragment extends Fragment implements LocationSource,AMapLocatio
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
             View view =  inflater.inflate(R.layout.fragment_home,container,false);
+            fenxiang = view.findViewById(R.id.fenxiang);
+            fenxiang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showShare();
+                }
+            });
             mapView = view.findViewById(R.id.map);
             mapView.onCreate(savedInstanceState);
             //管理地图方法
@@ -70,6 +81,26 @@ public class HomeFragment extends Fragment implements LocationSource,AMapLocatio
 
     }
 
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，微信、QQ和QQ空间等平台使用
+        oks.setTitle("分享");
+        // titleUrl QQ和QQ空间跳转链接
+          oks.setTitleUrl("http://www.baidu.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        // oks.setImagePath(R.drawable.logo2);//确保SDcard下面存在此张图片
+        // url在微信、微博，Facebook等平台中使用
+        oks.setUrl("http://www.baidu.com");
+        // comment是我对这条分享的评论，仅在人人网使用
+        oks.setComment("我是测试评论文本");
+        // 启动分享GUI
+        oks.show(getActivity());
+    }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
